cc19abfc25d5c5ba73c6eaac2664e70bd1157be1
KIMNAMMIN <namminkk@gmail.com>
2015-07-28 19:47:56 +0900

    Update MultiplicativePersistence.rb

--- MultiplicativePersistence.rb
+++ MultiplicativePersistence.rb
@@ -1,20 +1,21 @@
-def MultiplicativePersistence(num)
-    def M(num)
-	  str = Array.new
-      str = num.to_s
-      res = 1
-      i = 0 
-      cnt = 0
-      for i in 0..str.length-1
-        res = res * str[i].chr.to_i
-      end
+def M(num)
+  str = Array.new
+  str = num.to_s
+  res = 1
+  i = 0 
+  cnt = 0
+  for i in 0..str.length-1
+    res = res * str[i].chr.to_i
+  end
 
-      if res < 10
-        return 1
-      else
-        return M(res)+cnt+1
-      end
-    end 
+  if res < 10
+    return 1
+  else
+    return M(res)+cnt+1
+  end
+end
+
+def MultiplicativePersistence(num)    
   # code goes here
   str = Array.new
   str = num.to_s
@@ -31,7 +32,7 @@ def MultiplicativePersistence(num)
     return M(res)+cnt+1
   end
 end
-   
+
 # 초기 조건이 39일때는 3이고 4일때 0인게 엇갈려서(return 이 1일때 0일때가 엇갈림) 함수를 그냥 하나 더 구현했다.
 # 재귀느낌이 확 와서 이렇게 썼다.
 MultiplicativePersistence(STDIN.gets)          
