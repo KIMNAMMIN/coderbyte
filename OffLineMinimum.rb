851aae6f93545b66354c70e34cc588f4fc3a5a42
KIMNAMMIN <namminkk@gmail.com>
2015-07-28 15:46:05 +0900

    Update OffLineMinimum.rb

--- OffLineMinimum.rb
+++ OffLineMinimum.rb
@@ -8,7 +8,6 @@ def OffLineMinimum(strArr)
   
   for i in 0..strArr.length-1
     arr[j]=strArr[i]
- 
     if strArr[i]=='E'
        arr.sort!
        res.push(arr.reverse!.pop)
@@ -16,12 +15,8 @@ def OffLineMinimum(strArr)
     end
   	   j = j + 1
   end
-  
-  
   return res * ','
-         
 end
-   
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
 OffLineMinimum(STDIN.gets)           
